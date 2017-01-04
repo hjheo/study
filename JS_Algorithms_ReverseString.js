f8d8aa99ac9fb5d6e4b3e00feed90850c3b3681b
hjheo <odnisra@naver.com>
2017-01-04 09:24:49 +0900

    solution accepted

---
+++ JS_Algorithms_ReverseString.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+    
+    let stringReverse = s.split("").reverse().join("")
+    
+    return stringReverse
+    
+};
